d65fbdb1dfe8274d1011951b757817aa36faa204
Vincze Peter <vinczepetertamas@gmail.com>
2021-02-15 07:23:18 +0100

    ab-testing directory to gitignore proxy for artisan serve

--- server.php
+++ server.php
@@ -10,6 +10,9 @@
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
+ini_set('display_errors',1);
+ini_set('error_reporting()',E_ALL);
+
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
@@ -18,4 +21,4 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
 
-require_once __DIR__.'/public/index.php';
+require_once __DIR__.'/public/abtestingproxy.php';
